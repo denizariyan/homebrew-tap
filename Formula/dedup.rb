9996e51668cc29befabe9042b7de89fcd42dc7bc
Deniz Ariyan <denizariyan@gmail.com>
2025-12-31 17:11:46 +0100

    Update dedup to v0.2.0

--- Formula/dedup.rb
+++ Formula/dedup.rb
@@ -1,8 +1,8 @@
 class Dedup < Formula
   desc "An extremely fast and efficient duplicate file finder"
   homepage "https://github.com/denizariyan/dedup"
-  url "https://github.com/denizariyan/dedup/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "ab9659d5ef3cf7bddf7b11577c43f08037f52fbbce2e372e0684a5b5d12aed61"
+  url "https://github.com/denizariyan/dedup/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2a8aa8ee76631803129336a9a72dca56a51657f03ea927dea0b631685a0ffd90"
   license "MIT"
 
   depends_on "rust" => :build
